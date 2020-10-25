02db623693ae62ea58ade4cdc7fed1cc8118e11c
penny0203 <71242419+penny0203@users.noreply.github.com>
2020-10-25 22:41:12 +0800

    Add files via upload

---
+++ Personnel.java
@@ -0,0 +1,12 @@
+package pack2;
+
+public class Personnel {    //∏∏¿‡
+    int number;
+    String name;
+    char sex;
+    public Personnel(int number, String name, char sex) {
+        this.number = number;
+        this.name = name;
+        this.sex = sex;
+    }
+}
